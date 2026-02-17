9b25b24cd4934ad64bba4c379f23012d39af6be5
iszyeffiong <israel.iszy2336@gmail.com>
2026-02-17 10:42:24 +0100

    mockup

--- mocks/empty.js
+++ mocks/empty.js
@@ -1,1 +1,22 @@
-export { };
+// Mock for @solana/web3.js and @solana/kit
+// This prevents build errors when these packages are missing but imported by dependencies
+
+export const Connection = class { };
+export const PublicKey = class { };
+export const Keypair = class { };
+export const Transaction = class { };
+export const SystemProgram = class { };
+export const LAMPORTS_PER_SOL = 1000000000;
+export const clusterApiUrl = () => "";
+export const sendAndConfirmTransaction = async () => { };
+
+export default {
+    Connection,
+    PublicKey,
+    Keypair,
+    Transaction,
+    SystemProgram,
+    LAMPORTS_PER_SOL,
+    clusterApiUrl,
+    sendAndConfirmTransaction
+};
